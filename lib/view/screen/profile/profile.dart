813092aec5f73d887f650fb26776a926b28fd83b
软-马-伟伦 <weilunonebest@outlook.com>
2020-03-13 15:19:12 +0800

    UI improve

--- lib/view/screen/profile/profile.dart
+++ lib/view/screen/profile/profile.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:learner/models/user.dart';
 import 'package:learner/models/user_profile.dart';
 import 'package:learner/services/database.dart';
+import 'package:learner/shared/loading.dart';
 import 'package:learner/view/screen/profile/screen/profileShowDetail.dart';
 import 'package:provider/provider.dart';
 
 class Profile extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
@@ -15,19 +15,55 @@ class Profile extends StatelessWidget {
     return FutureProvider<UserProfile>.value(
       value: DatabaseService(uid: user.uid).userProfile,
       child: Container(
-        child: Consumer<UserProfile>(
-          builder: (context, myProfile, child) {
-            return RaisedButton(onPressed: () {
-                      navigateToProfileShowDetail(context, myProfile);
-                    });
-          }
-        )
-      ),
+          child: Consumer<UserProfile>(builder: (context, myProfile, child) {
+          return myProfile == null
+            ? Loading()
+            : Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: CircleAvatar(
+                      radius: 25.0,
+                      backgroundColor: Colors.blue,
+                    ),
+                    title: Text(myProfile.name),
+                    subtitle: InkWell(
+                      child: Text(
+                        'View Profile',
+                        style: TextStyle(color: Colors.pinkAccent),
+                      ),
+                      onTap: () {
+                        navigateToProfileShowDetail(context, myProfile);
+                      },
+                    ),
+                    trailing: Icon(Icons.person_outline),
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                    thickness: 2,
+                  ),
+                  ListTile(
+                    title: Text('Personal information'),
+                    trailing: Icon(Icons.person_outline),
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                    indent: 20,
+                    endIndent: 20,
+                  ),
+                  ListTile(
+                    title: Text('Personal information'),
+                    trailing: Icon(Icons.person_outline),
+                  )
+                ],
+              );
+      })),
     );
   }
 
   void navigateToProfileShowDetail(context, UserProfile user) {
     Navigator.push(
-        context, MaterialPageRoute(builder: (context) => ProfileShowDetails(user: user)));
+        context,
+        MaterialPageRoute(
+            builder: (context) => ProfileShowDetails(user: user)));
   }
 }
--- lib/view/screen/profile/screen/profileShowDetail.dart
+++ lib/view/screen/profile/screen/profileShowDetail.dart
@@ -19,34 +19,55 @@ class _ProfileShowDetailsState extends State<ProfileShowDetails> {
     print('this widget:' + widget.user.joinedIn);
     return widget.user == null
         ? Loading()
-        : Material(child: Scaffold(
+        : Material(
+            child: Scaffold(
             backgroundColor: Colors.white,
             appBar: AppBar(
-              title: Text('Learner'),
+              iconTheme: IconThemeData(color: Colors.grey),
+              backgroundColor: Colors.white,
               elevation: 0.0,
-              // actions: <Widget>[
-              //   FlatButton.icon(
-              //       onPressed: () async {
-              //         navigateBack(context);
-              //       },
-              //       icon: Icon(Icons.exit_to_app),
-              //       label: Text('Logout'))
-              // ],
-            ),
-            body: Column(
-              children: <Widget>[
-                ListTile(
-                    trailing: CircleAvatar(
-                      radius: 25.0,
-                      backgroundColor: Colors.blue,
-                    ),
-                    title: Text('Hi, ' + widget.user.name,style: TextStyle(fontSize:20 ),),
-                    subtitle: Text(
-                        'Joined in ${getJoinMonth(widget.user.joinedIn)}, ${getJoinYear(widget.user.joinedIn)}',style: TextStyle(fontSize:20 ),)),
-                Divider(
-                  color: Colors.grey,
+              actions: <Widget>[
+                IconButton(
+                    onPressed: () async {
+                      //navigateBack(context);
+                    },
+                    icon: Icon(Icons.edit),
                 )
               ],
+            ),
+            body: Container(
+              margin: EdgeInsets.all(15),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                      trailing: CircleAvatar(
+                        radius: 25.0,
+                        backgroundColor: Colors.blue,
+                      ),
+                      title: Text(
+                        'Hi, ' + widget.user.name,
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      subtitle: Text(
+                        'Joined in ${getJoinMonth(widget.user.joinedIn)}, ${getJoinYear(widget.user.joinedIn)}',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      )),
+                  Divider(
+                    color: Colors.grey,
+                  ),
+                  ListTile(
+                    title: Text('About'),
+                    subtitle: ListTile(
+                      leading: Icon(Icons.home),
+                      title: Text('Lives in ${widget.user.location}'),
+                    ),
+                  )
+                ],
+              ),
             ),
           ));
   }
@@ -57,10 +78,5 @@ class _ProfileShowDetailsState extends State<ProfileShowDetails> {
 
   String getJoinYear(String userJoinIn) {
     return userJoinIn.substring(userJoinIn.length - 4, userJoinIn.length);
-  }
-
-  void navigateBack(context) {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => Profile()));
   }
 }
